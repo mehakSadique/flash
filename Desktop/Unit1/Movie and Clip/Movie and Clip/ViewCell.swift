484f65036a5ca6554c6b971700926ae9ceb73ec4
Mehak Sadique <mksd1999@gmail.com>
2019-10-18 20:10:03 -0400

    Hello

---
+++ Desktop/Unit1/Movie and Clip/Movie and Clip/ViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ViewCell.swift
+//  Movie and Clip
+//
+//  Created by Mehak Sadique on 10/18/19.
+//  Copyright © 2019 Mehak Sadique. All rights reserved.
+//
+
+import UIKit
+
+class ViewCell: UIViewTableCell {
+
+}
